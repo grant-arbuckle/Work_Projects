3b3dd2dc137d7a51ad2b813d80e8c080b67d7d0a
Grant Arbuckle <99680552+grant-arbuckle@users.noreply.github.com>
2022-05-23 20:39:33 -0400

    Delete Directory walk test.py

--- Directory walk test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-from os.path import join, getsize
-
-directory = '//in-fileserver/Business_Office/Month_End_Worksheets/POSTAGE/2022 Postage/'
-
-for files in os.walk(directory):
-  print(files)
